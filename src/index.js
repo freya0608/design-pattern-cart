8edf58e2c81cb7b3c51ad3a64a1f5615c1cb1b35
freya0608 <freya001@yeah.net>
2019-04-22 17:15:10 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import App from './demo/App.js'
+
+let app = new App('app')
+app.init()
+
+// console.log('hshsh8888');
+
+// class Person {
+//     constructor(name){
+//         this.name = name
+//     }
+//     getName(){
+//         return this.name
+//     }
+// }
+//
+// let p = new Person('freya');
+// console.log(p.getName());
